b4e234290f2460c175edcb9ffd59ced68ac7467a
Tiberiu Popescu <tiberiu1204@gmail.com>
2023-10-31 23:16:30 +0200

    Done some, broke some

--- client/src/Components/Board.jsx
+++ client/src/Components/Board.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import "./Board.css"
 
 const list = [
@@ -25,7 +25,13 @@ function getSquare(index) {
     return Math.floor((index - 1) / 9) + 1;
 }
 
-const Tile = ({ value, index, selected, setSelected }) => {
+const Tile = ({ index, selected, setSelected }) => {
+    const [value, setValue] = useState(0);
+
+    useEffect(() => {
+        setValue(list[getRow(index) - 1][getCol(index) - 1]);
+    }, list);
+
     function handleHighlight() {
         let square = getSquare(index);
         let row = getRow(index);
@@ -41,13 +47,23 @@ const Tile = ({ value, index, selected, setSelected }) => {
         }
         return "";
     }
+
+    const handleKeyPress = e => {
+        if("123456789".search(e.key) !== -1) {
+            setValue(e.key);
+            console.log("yes");
+        }
+    }
+
     return (
     <div 
         className={`sudoku-tile ${handleHighlight()}`} 
         onClick={() => {
             setSelected({index: index, value: value});
-        }
-    }>
+        }}
+        tabIndex={0} 
+        onKeyDown={handleKeyPress}
+        >
         <div className='sudoku-tile-value'>{value ? value : ""}</div>
     </div>
   )
@@ -62,17 +78,72 @@ const Square = ({ tiles, index, selected, setSelected }) => {
                         value={num} 
                         index={index * 9 + ind + 1}
                         selected={selected} 
-                        setSelected={setSelected}>
+                        setSelected={setSelected}
+                        >
                     </Tile>
             })}
         </div>
     )
 }
 
+const arr = new Array(9);
+
 export const Board = () => {
     const [selected, setSelected] = useState(0);
+
+    useEffect(() => {
+        let index = 0;
+        for(let i = 0; i < 9; i++) {
+            arr[i] = new Array(9);
+        }
+        for(let i = 0; i < 9; i++) {
+            for(let j = 0; j < 9; j++) {
+                arr[getRow(index + 1) - 1][getCol(index + 1) - 1] = index;
+                index++;
+            }
+        }
+    }, []);
+
+    const handleKeyPress = e => {
+        const { index } = selected;
+        let row, col;
+        if(!selected) return;
+        switch(e.key) {
+            case "ArrowUp":
+                row = getRow(index) - 2;
+                col = getCol(index) - 1;
+                if(row >= 0) {
+                    setSelected({index: arr[row][col] + 1, value: list[row][col]});
+                }
+                break;
+            case "ArrowRight":
+                row = getRow(index) - 1;
+                col = getCol(index);
+                if(col <= 8) {
+                    setSelected({index: arr[row][col] + 1, value: list[row][col]});
+                }
+                break;
+            case "ArrowDown":
+                row = getRow(index);
+                col = getCol(index) - 1;
+                if(row <= 8) {
+                    setSelected({index: arr[row][col] + 1, value: list[row][col]});
+                }
+                break;
+            case "ArrowLeft":
+                row = getRow(index) - 1;
+                col = getCol(index) - 2;
+                if(col >= 0) {
+                    setSelected({index: arr[row][col] + 1, value: list[row][col]});
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
     return (
-    <div className='sudoku-board'>
+    <div className='sudoku-board' tabIndex={0} onKeyDown={handleKeyPress}>
         {list.map((tiles, index) => {
             return <Square 
                     key={index} 
